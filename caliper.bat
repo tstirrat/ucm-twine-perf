620eb42e8198506a8602459bf20d04e07e490b99
Tim Stirrat <tim.stirrat@gmail.com>
2012-10-17 12:42:18 +1100

    windows batch updated to use setlocal
    This is so that for environment vars are not mangled outside of the
    batch

--- caliper.bat
+++ caliper.bat
@@ -1,4 +1,6 @@
 @echo off
+SETLOCAL
+
 REM class path based on maven dependencies
 SET CLASSPATH=lib\ucm-11.1.1.6.0.jar;lib\asm-3.3.1.jar;lib\asm-analysis-3.3.1.jar;lib\asm-commons-3.3.1.jar;lib\asm-tree-3.3.1.jar;lib\asm-util-3.3.1.jar;lib\asm-xml-3.3.1.jar;lib\caliper-0.5-rc1.jar;lib\commons-lang-2.6.jar;lib\gson-1.7.1.jar;lib\guava-11.0.1.jar;lib\java-allocation-instrumenter-2.0.jar;lib\jsr305-1.3.9.jar
 
